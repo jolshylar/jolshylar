2453144cdf6de305836d9b76e7eded729009b4a9
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-27 05:05:31 +0600

    fix(components): change deprecated color names

--- src/components/Topics.tsx
+++ src/components/Topics.tsx
@@ -3,7 +3,7 @@ export default function Topics({ topics }: any) {
     <div className="flex flex-wrap max-w-[260px]">
       {topics &&
         topics.map((topic: string) => (
-          <span className="m-1 text-[0.7em] p-1 border-[1px] border-orange rounded-md">
+          <span className="m-1 text-[0.7em] p-1 border-[1px] border-secondary rounded-md">
             {topic}
           </span>
         ))}
