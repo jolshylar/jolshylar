b430d506558f2768abf66eca5f7db949e51fbaeb
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-18 12:52:06 +0600

    feat(components): fixed navbar
    Co-authored-by: Bek Slambek <MugiD@users.noreply.github.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,23 +3,12 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-5">
+    <nav className="w-full flex justify-center fixed p-1 bg-black/30 z-50">
       <Link href="/">
-        <Image
-          className="cursor-pointer"
-          src="/images/logo.png"
-          height={51}
-          width={192}
-        />
+        <a>
+          <Image src="/images/logo.png" height={51} width={192} />
+        </a>
       </Link>
-      <div className="flex gap-4 font-bold text-primary text-xl sm:text-2xl">
-        <Link href="/projects">
-          <a>Projects</a>
-        </Link>
-        <Link href="/members">
-          <a>Members</a>
-        </Link>
-      </div>
     </nav>
   );
 }
