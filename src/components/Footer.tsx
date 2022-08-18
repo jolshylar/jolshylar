14d42bfdef0b8511a87e83bba7e0ea816b368804
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-18 12:52:23 +0600

    feat(components): redesign footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,41 +1,47 @@
+import {
+  IoLogoTwitter,
+  IoLogoInstagram,
+  IoLogoGithub,
+  IoLogoDiscord,
+  IoMail,
+} from "react-icons/io5";
+
 export default function Footer() {
+  const iconStyles = { width: 24, height: 24 };
   return (
-    <footer className="text-center border-t-2">
-      <ul className="flex justify-around py-4">
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/jolshylar"
-          >
-            Github
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://discord.com/invite/6dSpCGtJuB"
-          >
-            Discord
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://instagram.com/jolshylar"
-          >
-            Instagram
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="mailto:jolshylar@gmail.com">
-            E-mail
-          </a>
-        </li>
-      </ul>
-      <div className="py-4">&copy; Jolshylar 2022. All rights reserved.</div>
+    <footer>
+      <div className="flex justify-center gap-4 py-4">
+        <a target="_blank" rel="noreferrer" href="https://github.com/jolshylar">
+          <IoLogoGithub style={iconStyles} />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://discord.com/invite/6dSpCGtJuB"
+        >
+          <IoLogoDiscord style={iconStyles} />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://twitter.com/jolshylar"
+        >
+          <IoLogoTwitter style={iconStyles} />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://instagram.com/jolshylar"
+        >
+          <IoLogoInstagram style={iconStyles} />
+        </a>
+        <a target="_blank" rel="noreferrer" href="mailto:jolshylar@gmail.com">
+          <IoMail style={iconStyles} />
+        </a>
+      </div>
+      <div className="text-center py-4">
+        &copy; Jolshylar 2022. All rights reserved.
+      </div>
     </footer>
   );
 }
