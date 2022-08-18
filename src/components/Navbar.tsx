7ae379bb36643bff654ecee399cc6d4620196ed4
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-08-18 14:02:03 +0600

    fix(components): change navbar transparency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex justify-center fixed p-1 bg-black/30 z-50">
+    <nav className="w-full flex justify-center fixed p-1 bg-black/60 z-50">
       <Link href="/">
         <a>
           <Image src="/images/logo.png" height={51} width={192} />
